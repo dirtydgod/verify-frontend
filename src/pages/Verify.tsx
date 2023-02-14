aa6997c496cbce83841092368b13889a49294534
extrahash <ExtraHash@protonmail.com>
2023-02-13 18:56:50 -0700

    remove frenly faces specific branding

--- src/pages/Verify.tsx
+++ src/pages/Verify.tsx
@@ -60,122 +60,99 @@ export function Verify() {
             }}
         >
             <Navbar RightElement={<ConnectButton showBalance={false} />} />
-            <div>
-                <img
-                    style={{
-                        width: "400px",
-                        maxWidth: "80%",
-                        zIndex: 1000,
-                        margin: "auto",
-                    }}
-                    src={eye}
-                    alt="welcome to frenly faces"
-                ></img>
-            </div>
-            {!success && !accessToken && (
-                <div>
-                    <div style={{ zIndex: 1000 }}>
-                        <p
-                            style={{
-                                textAlign: "center",
-                                fontSize: 24,
-                                fontFamily: "monospace",
-                                marginBottom: "2rem",
-                            }}
-                        >
-                            please{" "}
-                            <button
-                                className="underline text-pink-600 font-bold"
-                                onClick={() => {
-                                    window.location.href = `https://discord.com/api/oauth2/authorize?client_id=1071113835659923466&redirect_uri=${encodeURIComponent(
-                                        process.env
-                                            .REACT_APP_DISCORD_REDIRECT_URI!
-                                    )}&response_type=token&scope=identify`;
+
+            <div className="mt-10">
+                {!success && !accessToken && (
+                    <div>
+                        <div style={{ zIndex: 1000 }}>
+                            <h1
+                                style={{
+                                    textAlign: "center",
+                                    fontSize: 32,
+                                    fontFamily: "monospace",
+                                    marginBottom: "2rem",
                                 }}
                             >
-                                verify your Discord
-                            </button>{" "}
-                            to be Seen
-                        </p>
-                    </div>
-                </div>
-            )}
-            {!success && accessToken && user && (
-                <div>
-                    <div style={{ zIndex: 1000 }}>
-                        <p
-                            style={{
-                                textAlign: "center",
-                                fontSize: 24,
-                                fontFamily: "monospace",
-                                marginBottom: "2rem",
-                            }}
-                        >
-                            <p>
-                                we See you{" "}
-                                <strong>
-                                    {user.username}#{user.discriminator}
-                                </strong>
+                                {process.env.REACT_APP_PROJECT_NAME} NFT
+                                Verification
+                            </h1>
+                            <p
+                                style={{
+                                    textAlign: "center",
+                                    fontSize: 24,
+                                    fontFamily: "monospace",
+                                    marginBottom: "2rem",
+                                }}
+                            >
+                                You've been asked to verify holder status for{" "}
+                                {process.env.REACT_APP_PROJECT_NAME}.
+                                Please&nbsp;
+                                <button
+                                    className="underline text-pink-600 font-bold"
+                                    onClick={() => {
+                                        window.location.href = `https://discord.com/api/oauth2/authorize?client_id=1071113835659923466&redirect_uri=${encodeURIComponent(
+                                            process.env
+                                                .REACT_APP_DISCORD_REDIRECT_URI!
+                                        )}&response_type=token&scope=identify`;
+                                    }}
+                                >
+                                    verify your Discord
+                                </button>{" "}
+                                account to begin the process.
                             </p>
-
-                            {isDisconnected && (
-                                <p style={{ marginTop: "1rem" }}>
-                                    please{" "}
-                                    <button
-                                        onClick={openConnectModal}
-                                        className="underline text-pink-600 font-bold"
-                                    >
-                                        connect your wallet
-                                    </button>{" "}
-                                    to continue
+                        </div>
+                    </div>
+                )}
+                {!success && accessToken && user && (
+                    <div>
+                        <div style={{ zIndex: 1000 }}>
+                            <p
+                                style={{
+                                    textAlign: "center",
+                                    fontSize: 24,
+                                    fontFamily: "monospace",
+                                    marginBottom: "2rem",
+                                }}
+                            >
+                                <p>
+                                    Your Discord account was successfully
+                                    authenticated.
                                 </p>
-                            )}
-                            {!isDisconnected && address && (
+
+                                <img
+                                    style={{ marginTop: "1rem" }}
+                                    src={`https://cdn.discordapp.com/avatars/${user.id}/${user.avatar}.jpeg`}
+                                    className="rounded-full mx-auto h-48"
+                                />
                                 <p style={{ marginTop: "1rem" }}>
-                                    please{" "}
-                                    <button
-                                        onClick={async () => {
-                                            const res = await fetch(
-                                                `https://api.frenlyfaces.xyz/verify/token`,
-                                                {
-                                                    method: "POST",
-                                                    body: JSON.stringify({
-                                                        address,
-                                                    }),
-                                                    headers: {
-                                                        "Content-Type":
-                                                            "application/json",
-                                                    },
-                                                }
-                                            );
+                                    <strong>{user.username}</strong>#
+                                    {user.discriminator}
+                                </p>
 
-                                            if (res.status === 200) {
-                                                const {
-                                                    token,
-                                                }: { token: string } =
-                                                    await res.json();
-
-                                                const result =
-                                                    await signMessageAsync({
-                                                        message: token,
-                                                    });
-
-                                                const payload = {
-                                                    message: token,
-                                                    signature: result,
-                                                    discordToken: accessToken,
-                                                    discordTokenType: tokenType,
-                                                };
-
-                                                console.log(payload);
-
-                                                const verifyRes = await fetch(
-                                                    `https://api.frenlyfaces.xyz/verify/verify`,
+                                {isDisconnected && (
+                                    <p style={{ marginTop: "1rem" }}>
+                                        please{" "}
+                                        <button
+                                            onClick={openConnectModal}
+                                            className="underline text-pink-600 font-bold"
+                                        >
+                                            connect your wallet
+                                        </button>{" "}
+                                        to continue
+                                    </p>
+                                )}
+                                {!isDisconnected && address && (
+                                    <p style={{ marginTop: "1rem" }}>
+                                        Please{" "}
+                                        <button
+                                            onClick={async () => {
+                                                const res = await fetch(
+                                                    `https://api.frenlyfaces.xyz/verify/token`,
                                                     {
                                                         method: "POST",
-                                                        body: JSON.stringify(
-                                                            payload
-                                                        ),
+                                                        body: JSON.stringify({
+                                                            address,
+                                                        }),
                                                         headers: {
                                                             "Content-Type":
                                                                 "application/json",
@@ -183,52 +160,93 @@ export function Verify() {
                                                     }
                                                 );
 
-                                                if (verifyRes.status === 200) {
-                                                    setSuccess(true);
+                                                if (res.status === 200) {
+                                                    const {
+                                                        token,
+                                                    }: { token: string } =
+                                                        await res.json();
+
+                                                    const result =
+                                                        await signMessageAsync({
+                                                            message: token,
+                                                        });
+
+                                                    const payload = {
+                                                        message: token,
+                                                        signature: result,
+                                                        discordToken:
+                                                            accessToken,
+                                                        discordTokenType:
+                                                            tokenType,
+                                                    };
+
+                                                    console.log(payload);
+
+                                                    const verifyRes =
+                                                        await fetch(
+                                                            `https://api.frenlyfaces.xyz/verify/verify`,
+                                                            {
+                                                                method: "POST",
+                                                                body: JSON.stringify(
+                                                                    payload
+                                                                ),
+                                                                headers: {
+                                                                    "Content-Type":
+                                                                        "application/json",
+                                                                },
+                                                            }
+                                                        );
+
+                                                    if (
+                                                        verifyRes.status === 200
+                                                    ) {
+                                                        setSuccess(true);
+                                                    } else {
+                                                        alert(
+                                                            "We couldn't verify you as a frenly face. Ensure you've connected the right wallet. If you continue to experience problems, DM us on Twitter @FrenlyFaces"
+                                                        );
+                                                    }
                                                 } else {
-                                                    alert(
-                                                        "We couldn't verify you as a frenly face. Ensure you've connected the right wallet. If you continue to experience problems, DM us on Twitter @FrenlyFaces"
-                                                    );
+                                                    alert(res.statusText);
                                                 }
-                                            } else {
-                                                alert(res.statusText);
-                                            }
-                                        }}
-                                        className="underline text-pink-600 font-bold"
-                                    >
-                                        sign a message
-                                    </button>{" "}
-                                    to verify your membership
-                                </p>
-                            )}
+                                            }}
+                                            className="underline text-pink-600 font-bold"
+                                        >
+                                            sign a message
+                                        </button>{" "}
+                                        to verify your holder status for{" "}
+                                        {process.env.REACT_APP_PROJECT_NAME}
+                                    </p>
+                                )}
+                            </p>
+                        </div>
+                    </div>
+                )}
+                {success && (
+                    <div style={{ zIndex: 1000 }}>
+                        <p
+                            style={{
+                                textAlign: "center",
+                                fontSize: 24,
+                                fontFamily: "monospace",
+                                marginBottom: "2rem",
+                            }}
+                        >
+                            Your Holder status has been verified!
+                        </p>
+                        <p
+                            style={{
+                                textAlign: "center",
+                                fontSize: 24,
+                                fontFamily: "monospace",
+                                marginBottom: "2rem",
+                            }}
+                        >
+                            You may now return to Discord.
                         </p>
                     </div>
-                </div>
-            )}
-            {success && (
-                <div style={{ zIndex: 1000 }}>
-                    <p
-                        style={{
-                            textAlign: "center",
-                            fontSize: 24,
-                            fontFamily: "monospace",
-                            marginBottom: "2rem",
-                        }}
-                    >
-                        you have been Seen
-                    </p>
-                    <p
-                        style={{
-                            textAlign: "center",
-                            fontSize: 24,
-                            fontFamily: "monospace",
-                            marginBottom: "2rem",
-                        }}
-                    >
-                        you may return to discord
-                    </p>
-                </div>
-            )}
+                )}
+            </div>
         </div>
     );
 }
